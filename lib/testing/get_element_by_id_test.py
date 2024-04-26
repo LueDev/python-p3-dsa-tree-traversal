d5dfe1d1360d1df50ea2d90fbdd613c972705586
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-04-26 19:41:33 -0400

    Updated the test to include one for found node with matching id value passed to the function. All tests passing. Looking at Dijkstras algo too

--- lib/testing/get_element_by_id_test.py
+++ lib/testing/get_element_by_id_test.py
@@ -4,12 +4,12 @@ class TestStack:
     '''Class Stack in Stack.py'''
 
     def test_get_element_by_id(self):
-  # <body>
-  #   <div id="main">
-  #     <h1 id="heading">Title</h1>
-  #     <h2>Subtitle</h2>
-  #   </div>
-  # </body>
+    # <body>
+    #   <div id="main">
+    #     <h1 id="heading">Title</h1>
+    #     <h2>Subtitle</h2>
+    #   </div>
+    # </body>
 
         '''get_element_by_id test'''
     tree = Tree(
@@ -38,11 +38,54 @@ class TestStack:
         ]
       }
     )
-    assert(tree.get_element_by_id('heading') =={
-      'tag_name': 'h1',
-      'id': 'heading',
-      'text_content': 'Title',
-      'children': []
-    } )
+    # assert(tree.get_element_by_id('heading') =={
+    #   'tag_name': 'h1',
+    #   'id': 'heading',
+    #   'text_content': 'Title',
+    #   'children': []
+    # } )
 
     assert(tree.get_element_by_id('nope') == None)
+
+
+    def test_get_element_by_id_not_null(self):
+      # <body>
+      #   <div id="main">
+      #     <h1 id="heading">Title</h1>
+      #     <h2>Subtitle</h2>
+      #   </div>
+      # </body>
+
+          '''get_element_by_id test'''
+          tree = Tree(
+            {
+              'tag_name': 'body',
+              'id': None,
+              'children': [
+                {
+                  'tag_name': 'div',
+                  'id': 'main',
+                  'children': [
+                    {
+                      'tag_name': 'h1',
+                      'id': 'heading',
+                      'text_content': 'Title',
+                      'children': []
+                    },
+                    {
+                      'tag_name': 'h2',
+                      'id': None,
+                      'text_content': 'Subitle',
+                      'children': []
+                    }
+                  ]
+                }
+              ]
+            }
+          )
+          assert(tree.get_element_by_id('heading') =={
+            'tag_name': 'h1',
+            'id': 'heading',
+            'text_content': 'Title',
+            'children': []
+          } )
--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,14 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    if self.root: 
+      nodes_to_visit = [self.root]
+      while len(nodes_to_visit) > 0:
+        node = nodes_to_visit.pop(0)
+        if node['id'] != id:
+          nodes_to_visit = node['children'] + nodes_to_visit
+        else: 
+          return node
+    
+    return None
+      
